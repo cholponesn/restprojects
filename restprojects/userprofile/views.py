7570f0d0bd3e8622bc9168f78a3ae4814df5f8ad
cholponesn <cholponkaimova653@example.com>
2021-06-15 18:24:39 +0600

    ProfileView(APIView):

---
+++ restprojects/userprofile/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.views import APIView
+
+class ProfileView(APIView):
+
+    def get(self,request):
+        try:
+            profile = Profile.objects.get(user=request.user)
+        except Profile.DoesNotExist:
+            return Response('404', status=status.HTTP_404_NOT_FOUND)
+        serializers = ProfileSerializer(profile)
+        return Response(serializers.data,status=status.HTTP_200_OK)
+
+
+
