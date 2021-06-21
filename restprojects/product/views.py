3562e85762cf55729b48e583f7d23b70da0822ac
cholponesn <cholponkaimova653@example.com>
2021-06-21 18:23:44 +0600

    add class CategoryProductView

--- restprojects/product/views.py
+++ restprojects/product/views.py
@@ -10,3 +10,11 @@ class CategoryView(APIView):
         category = Category.objects.all()
         serializers = CategorySerializer(category,many=True)
         return Response(serializers.data,status=status.HTTP_200_OK)
+
+class CategoryProductView(APIView):
+
+    def get(self,request,*args,**kwargs):
+        category = Category.objects.get(title=kwargs['cat_title'])
+        product_all = category.product_set.all()
+        serializer = ProductSerializer(product_all,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
--- restprojects/userprofile/views.py
+++ restprojects/userprofile/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework.views import APIView
+from .services import *
 
 class ProfileView(APIView):
-
     def get(self,request):
         try:
             profile = Profile.objects.get(user=request.user)
@@ -13,6 +14,43 @@ class ProfileView(APIView):
             return Response('404', status=status.HTTP_404_NOT_FOUND)
         serializers = ProfileSerializer(profile)
         return Response(serializers.data,status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+
+    def post(self,request,):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Successfuly register!',status=201)
+        return Response(serializer.errors,status=400)
+
+class LoginView(APIView):
+
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializers.data.get('username')
+            password = serializers.data.get('password')
+            user = authenticate(username=username,password=password)
+            login(request,user)
+
+            return Response('welcome')
+        return Response(serializers.errors)
+
+    def put(self, request):
+        profile = Profile.objects.get(user=request.user)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('OK', status=200)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request):
+        profile = Profile.objects.get(user=request.user)
+        profile.delete()
+        return Response('DELETED', status=200)
+
 
 
 
